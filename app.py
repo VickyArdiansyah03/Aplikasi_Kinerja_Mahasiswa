919c1c22dfb900d0d35905a269e3c98168a200d9
VickyArdiansyah03 <ardiansyahvicky308@gmail.com>
2025-06-16 00:23:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -173,8 +173,10 @@ def load_login_user_data(filepath, id_column="ID"):
 
 def render_header():
     st.markdown("""
-        <h2 style='text-align: center; color: #4CAF50;'>📊 Sistem Informasi Kinerja Mahasiswa</h2>
-        <hr style='margin-top: -10px;'>
+        <div style="text-align:center;">
+            <h2 style="color:#4CAF50;">📊 Dashboard Kinerja Mahasiswa</h2>
+            <hr style="margin-top:-10px;">
+        </div>
     """, unsafe_allow_html=True)
     
 def logout():
